0fbeaba660fab535b7ed03965ee46cdcc4777633
Smith <linkraft@ufl.edu>
2020-10-02 23:29:49 -0400

    Added packages

---
+++ Assets/Scripts/FirebaseInit.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Firebase;
+using Firebase.Analytics;
+
+public class FirebaseInit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
+            FirebaseAnalytics.SetAnalyticsCollectionEnabled(true);
+        });
+    }
+}
