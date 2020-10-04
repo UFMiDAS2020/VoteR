043dbdf43f0d369c7eaac2681bc96b7c10bb9208
inessaid <43462511+inessaid@users.noreply.github.com>
2020-10-03 21:10:39 -0400

    settings picture

---
+++ Assets/startVR.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class startVR : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+
+    {
+
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        StartCoroutine(LoadDevice("cardboard"));
+
+        XRSettings.enabled = true;
+
+    }
+
+
+
+    IEnumerator LoadDevice(string newDevice)
+
+    {
+
+        XRSettings.LoadDeviceByName(newDevice);
+
+        yield return null;
+
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+        XRSettings.enabled = true;
+
+    }
+}
